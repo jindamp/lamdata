941438572c5828f01d8faeb901407ac308bd7e5c
pavan <jindmpavan@gmail.com>
2020-04-02 17:51:53 +0530

    Updates

---
+++ precache-manifest.bf43774931f3e3e0a73b59b41822a918.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37369bb1ef8f8c01b31dd09ccf3226ef",
+    "url": "/lamdata/index.html"
+  },
+  {
+    "revision": "281b68b0a7d8f754f432",
+    "url": "/lamdata/static/css/main.6f60fe5f.chunk.css"
+  },
+  {
+    "revision": "f9bb483eecff93d96d4c",
+    "url": "/lamdata/static/js/2.a253baa7.chunk.js"
+  },
+  {
+    "revision": "1d9e7ac4be56c1398ae3ecc8d596f5d1",
+    "url": "/lamdata/static/js/2.a253baa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281b68b0a7d8f754f432",
+    "url": "/lamdata/static/js/main.b0e1d20e.chunk.js"
+  },
+  {
+    "revision": "e318fe25e68f3e13842f",
+    "url": "/lamdata/static/js/runtime-main.04750d8b.js"
+  },
+  {
+    "revision": "c4b59ab516da45ece289cee9eb750290",
+    "url": "/lamdata/static/media/error_img.c4b59ab5.svg"
+  },
+  {
+    "revision": "43bdc870eb3561b52e7b46e708325c5f",
+    "url": "/lamdata/static/media/loading_img.43bdc870.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jindamp/lamdata/precache-manifest.41ab9d515c84daac51797d238fb992c3.js"
+  "/lamdata/precache-manifest.bf43774931f3e3e0a73b59b41822a918.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jindamp/lamdata/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lamdata/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
